3c41d70d974a3ca39ac6e4c2e5934d4b81ab601f
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-09-14 10:10:07 +0300

    Update index

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -11,7 +11,7 @@ const Section = ({ title, children }) => {
 
 Section.propTypes = {
   title: PropTypes.string,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.object.isRequired,
 };
 
 export default Section;
